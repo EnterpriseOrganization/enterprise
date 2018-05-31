ae7508dc627d688f28b58e7ac5b2676188118f0a
unknown <daerwendeshu@outlook.com>
2018-05-31 22:25:58 +0800

    增加了对应的后端代码. Ref #18.

--- backend/app_bom_permission/urls.py
+++ backend/app_bom_permission/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 import app_bom_permission.views as bom_view
 
 urlpatterns = [
-    url(r'^material/$', bom_view.process_material),
+    url(r'^material/$', bom_view.MaterialProcessor.process_material),
+    url(r'^material/([0-9]*)/$', bom_view.MaterialProcessor.process_specific_material)
 ]
