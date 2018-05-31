9332ba83ced772a2b2688fed9e68355adc96309a
Myh <15222751216@163.com>
2018-05-31 14:56:24 +0800

    add  all interface and demo, complete function get_quotation_list

--- backend/app_purchase/urls.py
+++ backend/app_purchase/urls.py
@@ -4,4 +4,14 @@ from . import views
 urlpatterns = [
 	url(r'^hello/', views.hello),
 	url(r'^get_quotation_list/', views.get_quotation_list),
+	url(r'^add_quotation/', views.add_quotation),
+	url(r'^delete_quotation/', views.delete_quotation),
+	url(r'^update_quotation_price/', views.update_quotation_price),
+	url(r'^quotation_query/', views.quotation_query),
+	url(r'^get_lack_list/', views.get_lack_list),
+	url(r'^add_purchase/', views.add_purchase),
+	url(r'^delete_purchases/', views.delete_purchases),
+	url(r'^get_purchase_list/', views.get_purchase_list),
+	url(r'^get_purchase_detail/', views.get_purchase_detail),
+	url(r'^purchase_query/', views.purchase_query),
 ]
--- backend/app_purchase/views.py
+++ backend/app_purchase/views.py
@@ -21,25 +21,118 @@ def get_quotation_list(request):
 	if request.method == 'POST':
 		sms = SupplierMaterial.objects.select_related('supplier', 'material').all()
 		# fileds = SupplierMaterial._meta.get_fields()
-		print(sms)
+		result = []
 		for sm in sms:
-			print(sm.supplier)
-			print(sm.material)
-		result = list(sms)
+			res = {
+				'id': sm.id,
+				'material_name': sm.material.name,
+				'price': sm.price,
+				'supplier_name':sm.supplier.name,
+			}
+			print(res)
+			result.append(res)
 
+		return JsonResponse({'msg': 200, 'result': result})
+	else:
+		return JsonResponse({'msg': 'Please use POST', 'result': 'null'})
+
+
+@method_decorator(csrf_exempt)
+def add_quotation(request):
+	"""
+	"""
+	if request.method == 'POST':
 		return JsonResponse({'msg': 200, 'result': 'ok'})
 	else:
 		return JsonResponse({'msg': 'Please use POST', 'result': 'null'})
 
 
 @method_decorator(csrf_exempt)
-def history_list(request):
+def delete_quotation(request):
+	"""
+	"""
 	if request.method == 'POST':
-		params = request.POST
-		dt=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-		supplier = Supplier.objects.get(id=1)
-		purchase = Purchase(purchaser='myh', date=dt, checker='leo', supplier=supplier)
-		purchase.save()
-		return JsonResponse({'msg':200})
+		return JsonResponse({'msg': 200, 'result': 'ok'})
+	else:
+		return JsonResponse({'msg': 'Please use POST', 'result': 'null'})
+
+
+@method_decorator(csrf_exempt)
+def quotation_query(request):
+	"""
+	"""
+	if request.method == 'POST':
+		return JsonResponse({'msg': 200, 'result': 'ok'})
+	else:
+		return JsonResponse({'msg': 'Please use POST', 'result': 'null'})
+
+
+@method_decorator(csrf_exempt)
+def update_quotation_price(request):
+	"""
+	"""
+	if request.method == 'POST':
+		return JsonResponse({'msg': 200, 'result': 'ok'})
+	else:
+		return JsonResponse({'msg': 'Please use POST', 'result': 'null'})
+
+
+@method_decorator(csrf_exempt)
+def get_lack_list(request):
+	"""
+	"""
+	if request.method == 'POST':
+		return JsonResponse({'msg': 200, 'result': 'ok'})
+	else:
+		return JsonResponse({'msg': 'Please use POST', 'result': 'null'})
+
+
+@method_decorator(csrf_exempt)
+def add_purchase(request):
+	"""
+	"""
+	if request.method == 'POST':
+		return JsonResponse({'msg': 200, 'result': 'ok'})
+	else:
+		return JsonResponse({'msg': 'Please use POST', 'result': 'null'})
+
+
+@method_decorator(csrf_exempt)
+def delete_purchases(request):
+	"""
+	"""
+	if request.method == 'POST':
+		return JsonResponse({'msg': 200, 'result': 'ok'})
+	else:
+		return JsonResponse({'msg': 'Please use POST', 'result': 'null'})
+
+
+@method_decorator(csrf_exempt)
+def get_purchase_list(request):
+	"""
+	"""
+	if request.method == 'POST':
+		return JsonResponse({'msg': 200, 'result': 'ok'})
+	else:
+		return JsonResponse({'msg': 'Please use POST', 'result': 'null'})
+
+
+@method_decorator(csrf_exempt)
+def get_purchase_detail(request):
+	"""
+	"""
+	if request.method == 'POST':
+		return JsonResponse({'msg': 200, 'result': 'ok'})
+	else:
+		return JsonResponse({'msg': 'Please use POST', 'result': 'null'})
+
+
+@method_decorator(csrf_exempt)
+def purchase_query(request):
+	"""
+	"""
+	if request.method == 'POST':
+
+		return JsonResponse({'msg': 200, 'result': 'ok'})
 	else:
 		return JsonResponse({'msg': 'Please use POST', 'result': 'null'})
