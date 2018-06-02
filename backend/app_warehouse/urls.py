c9f47682a83b1bb004b9090076ac2102076b8bcc
william <2444870620@qq.com>
2018-06-02 23:51:28 +0800

    Modify the query of warehouse. #59

--- backend/app_warehouse/urls.py
+++ backend/app_warehouse/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views_warehouse
 urlpatterns = [
-	url(r'^getInventor$',views_warehouse.getInventor),
+	url(r'^getInventor$',views_warehouse.getInventory),
+        url(r'^new-inventory',views_warehouse.addInventory),
 ]
--- backend/app_warehouse/views_warehouse.py
+++ backend/app_warehouse/views_warehouse.py
@@ -5,16 +5,17 @@ import time
 from enterprise.models import *
 from enterprise.DataScript import generateInventorInformation
 from django.core import serializers
+from django.views.decorators.csrf import ensure_csrf_cookie
 # Create your views here.
 from collections import defaultdict
-def getInventor(req):
-	print(req)
+import json
+def getInventory(req):
+	queryset = getParams(req)
 	anslist = InventoryInformation.objects.all()
 	answer = defaultdict(dict)
 	count = 0
 	for record in anslist:
 		material = record.material
-		#print("material is ", material.name)
 		temp = {
 			'id':material.id,
 			'name':material.name,
@@ -25,14 +26,35 @@ def getInventor(req):
 		answer.setdefault(count,temp)
 		count+=1
 	return JsonResponse(answer)
-
-def addInventor(req):
-	
+@ensure_csrf_cookie
+def addInventory(req):
+	params = getParams(req)
+	print(params)
+	InventoryInformation.objects.create( material = params['material'], name =\
+	                                     params['name'], shelfnumber = params['shelfnumber'], number =\
+	                                     params['number'], newestinwarehousedate = time.localtime() )
 	return JsonResponse({'res':'add success!'})
 
-def modifyInventor(req):
-
+def modifyInventory(req):
+	params = getParams(req)
+	InventoryInformation.objects.filter()
 	return JsonResponse({'res':'modify success!'})
 
 def removeRecord(req):
 	return JsonResponse({'res':'remove success!'})
+
+
+
+def getParams(req):
+	if(req.method == 'GET'):
+		params = dict(req.GET)
+		return params
+	elif(req.method == 'POST'):
+		params = json.loads(req.body.decode('utf-8'))
+		return params
+	elif(req.method == 'DELETE'):
+		print("in delete")
+		params = json.loads(req.body.decode('utf-8'))
+		print(params)
+		return params
+	
