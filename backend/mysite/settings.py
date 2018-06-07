355af8b5caef2bd56f21698b6aee263db2b503de
NKCqx <892670992@qq.com>
2018-06-07 15:40:35 +0800

    set password to default

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'enterprise',
         'USER': 'root',
-        'PASSWORD':'vagrant',
+        'PASSWORD':'',
         'HOST':'127.0.0.1',
         'PORT':'3306',
     }
