44426e65aa6cda5e99c0741cf8015b043cc6e4af
root <CuttlefishXuan>
2018-06-07 15:02:11 +0800

    Password

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'enterprise',
         'USER': 'root',
-        'PASSWORD':'vagrant',
+        'PASSWORD':'1002',
         'HOST':'127.0.0.1',
         'PORT':'3306',
     }
