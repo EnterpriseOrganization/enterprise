bea809d3588330dabb85473c8c3de7f0f869d788
cocoonYH <1511467@mail.nankai.edu.cn>
2018-06-08 16:08:11 +0800

    Harvey, fix abug on Views/ProduceTask.py and modify this py file.

--- backend/app_product/Views/ProduceTask.py
+++ backend/app_product/Views/ProduceTask.py
@@ -3,6 +3,7 @@ from django.utils.dateparse import parse_date
 from datetime import *
 import app_product.util as util
 
+
 # TODO: HyperMedia
 def getUnallocatedOrders():
     each_order = getProductsByOrderStatus(0, 0)
@@ -10,11 +11,11 @@ def getUnallocatedOrders():
 
 
 def getAllocatedOrders():
-    each_order = getProductsByOrderStatus(1, 3) # 先拿到所有已分配订单的货品信息
+    each_order = getProductsByOrderStatus(1, 3)  # 先拿到所有已分配订单的货品信息
     return each_order
 
 
-def getProductsByOrderStatus(gte, lte): # 想精确搜索就让 gte == lte
+def getProductsByOrderStatus(gte, lte):  # 想精确搜索就让 gte == lte
     """
     根据订单状态的范围返回相应每个订单包含的产品。要获取单一状态订单 只需使 gle=lte
     :param gte: 订单状态的最小值（大于等于）
@@ -33,15 +34,23 @@ def getProductsByOrderStatus(gte, lte): # 想精确搜索就让 gte == lte
         # orders[order_id] = orders.get(order_id, {"products": []})
         # 拿到了每个订单下的产品列表
         orders.append({
-            "order_id": order_id,
-            "status": product["order__status"],
+            "order_id":
+            order_id,
+            "status":
+            product["order__status"],
             #"deadline": product["order__deliverydate"],
             #"create_time": product["order__date"],
-            "product_id": product['product_id'],
-            "product_name": product['product__name'],
-            "amount": product["number"],
-            "tip": "comments",
-            "detail": util.MyServer + "product/tasks/byorder?order_id={}".format(order_id)
+            "product_id":
+            product['product_id'],
+            "product_name":
+            product['product__name'],
+            "amount":
+            product["number"],
+            "tip":
+            "comments",
+            "detail":
+            util.MyServer +
+            "product/tasks/byorder?order_id={}".format(order_id)
         })
     return orders
 
@@ -53,12 +62,13 @@ def queryTasks(params=None):
         tasks_list.append(util.wrapTaskDTO(task))
     return tasks_list
 
+
 def getTasksByTaskStatus(gte, lte):
     #tasks_list = queryTasks({"status__gte": gte, "status__lte": lte})
     tasks = ProduceTaskBasic.objects.filter(status__gte=gte, status__lte=lte)
-    tasks_DTO = []
+    tasks_list = []
     for task in tasks:
-        tasks_DTO.append(util.wrapTaskDTO(task))
+        tasks_list.append(util.wrapTaskDTO(task))
     return tasks_list
 
 
@@ -71,11 +81,13 @@ def getTasksByOrderID(order_id):
     }]
     """
     # tasks_list = queryTasks({"order_id": order_id})
-    tasks = ProduceTaskBasic.objects.select_related('workshop').filter(order_id=order_id)
+    tasks = ProduceTaskBasic.objects.select_related('workshop').filter(
+        order_id=order_id)
     tasks_list = []
     for task in tasks:
         tasks_list.append(util.wrapTaskDTO(task))
     return tasks_list
+
 
 def getTasksByWorkshopID(workshop_id):
     """
@@ -86,7 +98,8 @@ def getTasksByWorkshopID(workshop_id):
     }]
     """
     #tasks_list = queryTasks({"workshop_id": workshop_id})
-    tasks = ProduceTaskBasic.objects.select_related('workshop').filter(workshop_id=workshop_id)
+    tasks = ProduceTaskBasic.objects.select_related('workshop').filter(
+        workshop_id=workshop_id)
     tasks_list = []
     for task in tasks:
         tasks_list.append(util.wrapTaskDTO(task))
@@ -134,7 +147,8 @@ def createTasks(tasks, order_id=-1, from_outside=True):
             task = ProduceTaskBasic()
             # columns client must send
             if form.get("workshop_id") == None or form.get("amount") == None:
-                raise Exception(util.ErrMsg[0].format("missing important field: workshop_id or amount"))
+                raise Exception(util.ErrMsg[0].format(
+                    "missing important field: workshop_id or amount"))
 
             task.order_id = order_id
             task.workshop_id = form.get("workshop_id")
@@ -147,7 +161,9 @@ def createTasks(tasks, order_id=-1, from_outside=True):
             task.save()
             return util.wrapTaskDTO(task)
         else:
-            raise Exception(util.ErrMsg[0].format("need a list of tasks. create single task, please use 'createProduceTask'"))
+            raise Exception(util.ErrMsg[0].format(
+                "need a list of tasks. create single task, please use 'createProduceTask'"
+            ))
     else:
         raise Exception(util.ErrMsg[0].format("need specify a order"))
 
@@ -185,29 +201,35 @@ def getWorkshops(product_id=-1, all=False):
         return workshop_group
 
 
-
 def updateTask(task_id, form):
-    task =  ProduceTaskBasic.objects.get(pk=task_id)
+    task = ProduceTaskBasic.objects.get(pk=task_id)
     if type(form) == type({}):
-        if form.get("person_in_charge"): task.personincharge = form.get("person_in_charge")
+        if form.get("person_in_charge"):
+            task.personincharge = form.get("person_in_charge")
         if form.get("workshop_id"): task.workshop_id = form.get("workshop_id")
         if form.get("amount"): task.number = form.get("amount")
-        if form.get("deadline"): task.deadline = parse_date(form.get("deadline"))
+        if form.get("deadline"):
+            task.deadline = parse_date(form.get("deadline"))
         if form.get("topic"): task.topic = form.get("topic")
         if form.get("status"): task.status = form.get("status")
-        if form.get("material_getter"): task.material_getter = form.get("material_getter")
-        if form.get("material_checker"): task.material_checker = form.get("material_checker")
-        if form.get("material_distributon_date"): task.material_distributon_date = datetime.now().strftime("%Y-%m-%d %H:%I:%S")
+        if form.get("material_getter"):
+            task.material_getter = form.get("material_getter")
+        if form.get("material_checker"):
+            task.material_checker = form.get("material_checker")
+        if form.get("material_distributon_date"):
+            task.material_distributon_date = datetime.now().strftime(
+                "%Y-%m-%d %H:%I:%S")
         task.save()
         return util.wrapTaskDTO(task)
     else:
-        raise Exception(util.ErrMsg[0].format("updates info must in dict format"))
+        raise Exception(
+            util.ErrMsg[0].format("updates info must in dict format"))
 
 
 def deleteTask(task_id):
     task = ProduceTaskBasic.objects.get(pk=task_id)
     amount_tuple = task.delete()
-    return amount_tuple[0] # amounts of tasks being deleted
+    return amount_tuple[0]  # amounts of tasks being deleted
 
 
 def deleteTasksByOrderID(order_id):
@@ -218,8 +240,9 @@ def deleteTasksByOrderID(order_id):
 
 # 貌似没人用
 def deleteTasks(task_id_list):
-    if(type(task_id_list)== type([])):
+    if (type(task_id_list) == type([])):
         for task_id in task_id_list:
             deleteTask(task_id)
     else:
-        raise Exception(util.ErrMsg[0].format("using deleteTask to delete single task."))
+        raise Exception(
+            util.ErrMsg[0].format("using deleteTask to delete single task."))
