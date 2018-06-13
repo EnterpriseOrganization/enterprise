da185dd500edaa9023b7aade32ba60dfaa4a9745
OliverWang1226 <wangtingsheng1226@163.com>
2018-06-14 05:59:10 +0800

    Warehouse Issues solved

--- backend/app_warehouse/urls.py
+++ backend/app_warehouse/urls.py
@@ -4,5 +4,7 @@ urlpatterns = [
     url(r'^new-inventory',views_warehouse.addInventory),
     url(r'^inventory$',views_warehouse.getInventory),
     url(r'^remove-record',views_warehouse.removeRecord),
-    url(r'^modify-inventory',views_warehouse.modifyInventory)
+    url(r'^modify-inventory',views_warehouse.modifyInventory),
+    url(r'^get-inwarehouse-record',views_warehouse.getInWarehouseRecord),
+    url(r'^get-outwarehouse-record',views_warehouse.getOutWarehouseRecord),
 ]
