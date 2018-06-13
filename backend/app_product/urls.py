5a6ceb355ee8614b6664c3ebd78b4048cd3efd2b
NKCqx <892670992@qq.com>
2018-06-13 18:45:20 +0800

    add another url which also disappered

--- backend/app_product/urls.py
+++ backend/app_product/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'tasks$', ProduceTaskView.getAllTasks),
     url(r'tasks/real$', ProduceTaskView.getRealAllTasks),
     url(r'tasks/byorder$', ProduceTaskView.getOrderTasks),
+    url(r'tasks/byworkshop$', ProduceTaskView.getWorkshopTasks),
     url(r'tasks/history$', ProduceTaskView.getHistoryTasks),
     url(r'tasks/undone$', ProduceTaskView.getUndoneTasks),
     url(r'task/create$', ProduceTaskView.createTasks),
